7aaa0c414573e5039f3c9d7e03d45c4f3726505e
Franz Rohrhofer <frohrhofer@know-center.at>
2024-07-05 12:14:10 +0200

    Updated version with new structure, function definitions and plot

--- src/plots.py
+++ src/plots.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 
 
-def plot_input_data(X_IC, u_IC, X_BC, u_BC):
+def plot_datasets(X_IC, u_IC, X_BC, u_BC, X_col):
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     ax.scatter(X_IC[:,1], X_IC[:,0], u_IC[:,0], label='IC')
     ax.scatter(X_BC[:,1], X_BC[:,0], u_BC[:,0], label='BC')
+    ax.scatter(X_col[:,1], X_col[:,0], 0, label='col')
     
     ax.legend(frameon=False)
     
@@ -19,6 +19,23 @@ def plot_input_data(X_IC, u_IC, X_BC, u_BC):
     ax.set_ylabel('$x$')
     ax.set_zlabel('$u(x,t)$')
     
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_learning_curves(log_data, log_physics):
+    
+    epochs = range(len(log_data))
+
+    fig, ax = plt.subplots()
+
+    ax.plot(epochs, log_data, label='loss_data')
+    ax.plot(epochs, log_physics, label='loss_physics')
+
+    ax.set_yscale('log')
+    ax.legend(frameon=False)
+    ax.set_xlabel('Training Epoch')
+
     plt.tight_layout()
     plt.show()
     
@@ -40,6 +57,7 @@ def plot_prediction(model):
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    
     ax.plot_surface(tt, xx, u_pred, facecolors=cm.jet(u_pred/u_pred.max()), lw=0)
     
     ax.set_xlabel('$t$')
